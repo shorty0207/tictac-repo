da6b186dcc33853caded7038b2afd7b00b345bb8
shorty0207 <vytguc@gmail.com>
2020-11-29 17:40:32 +0200

    job done

--- code.js
+++ code.js
@@ -1,38 +1,80 @@
-let main = document.querySelector('#main')
-
+const container = document.querySelector(".container")
 
 let colors = []
-let realColors = [1,2,3,4,5,6,7,8]
 
-generateColor()
-makeSquares ()
+function generateGame (){
+    container.innerHTML = ""
 
-function makeSquares (){
-    for (let i = 0; i < 16; i++) {
+  //GENERATE COLORS
+    for (let i = 0; i < 9; i++) {
+        let rnd = "#"+((1<<24)*Math.random()|0).toString(16)
 
-
-        let randomColor = Math.floor( Math.random() * colors.length)
-        main.innerHTML += `
-        <div class="btn" id="${colors[randomColor]}" onclick="boxClicked(event,${i})"> </div>`
-        colors = colors.filter((item, index) => index !==randomColor)
-
+        colors.push(rnd, rnd)
     }
-}
 
-function boxClicked (event,i){
-
-
-    console.log(i)
-}
-
-function generateColor() {
-    for(let x = 0; x < 8; x++){
-        let randomColor = "#"+((1<<24)*Math.random()|0).toString(16)
-        colors.push(randomColor)
-    }
-    colors = [...colors,...colors]
     console.log(colors)
+
+    //GENERATE HTML
+    for (let i = 0; i <18 ; i++) {
+        let randomIndex = Math.floor(Math.random() * colors.length)
+
+        container.innerHTML +=`
+            <span class=" ${colors[randomIndex]}" onclick="clickBox(event)"></span>
+           
+        `
+        colors.splice(randomIndex, 1)
+    }
 }
+
+generateGame()
+
+let selected = {
+    one: null,
+    two: null,
+    counter: 0
+}
+
+function clickBox (event) {
+    let color = event.target.className
+
+    if (!event.target.classList.contains('selected')) {
+
+        console.log('works')
+        if (selected.counter === 0) {
+            selected.one = event.target
+            selected.one.style.backgroundColor = color
+            selected.one.classList.add("selected")
+            return selected.counter++
+        }
+        if (selected.counter === 1) {
+            selected.two = event.target
+            selected.two.style.backgroundColor = color
+            selected.two.classList.add("selected")
+            selected.counter++
+            checkColorMatch ()
+        }
+    }
+}
+
+
+function checkColorMatch () {
+    if (selected.one.className === selected.two.className){
+
+
+        selected.counter = 0
+    } else{
+        setTimeout(() =>{
+            selected.one.classList.remove("selected")
+            selected.two.classList.remove("selected")
+            selected.one.style.backgroundColor = '#bfc4c8'
+            selected.two.style.backgroundColor = '#bfc4c8'
+            selected.counter =0
+        },1000)
+
+    }
+}
+
+
 
 
 
